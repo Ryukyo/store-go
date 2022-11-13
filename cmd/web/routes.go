61ac00498e7b0ca06dcabb2d0d6b28fbccd3aae3
Ryukyo <florian.fejer@hotmail.de>
2022-11-13 21:41:33 +0900

    update routes and handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,12 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/general-quarters", handlers.Repo.Generals)
+	mux.Get("/suite", handlers.Repo.Suites)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileSever := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileSever))
