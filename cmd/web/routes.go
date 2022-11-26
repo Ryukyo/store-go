a331789bceabb70891fd7e5f6bc84103432af260
Ryukyo <florian.fejer@hotmail.de>
2022-11-26 22:27:54 +0900

    create route to post to availability from popup

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,8 +20,11 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/general-quarters", handlers.Repo.Generals)
 	mux.Get("/suite", handlers.Repo.Suites)
+
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
+
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
