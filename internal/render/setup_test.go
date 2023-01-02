670a6ea4b08f9ea8d46832c17502aca02447bd25
Ryukyo <florian.fejer@hotmail.de>
2023-01-02 17:55:33 +0900

    test render method

---
+++ internal/render/setup_test.go
@@ -0,0 +1,48 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Ryukyo/store-go/internal/config"
+	"github.com/Ryukyo/store-go/internal/models"
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	gob.Register(models.Reservation{})
+
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
+
+type testWriter struct{}
+
+func (tw *testWriter) Header() http.Header {
+	var h http.Header
+	return h
+}
+
+func (tw *testWriter) WriteHeader(i int) {}
+
+func (tw *testWriter) Write(b []byte) (int, error) {
+	length := len(b)
+	return length, nil
+}
